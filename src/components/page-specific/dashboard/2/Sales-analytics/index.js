43786d9f8acf05098fa8edba0c6208d9817a2989
ZeetGeek <150018728+ZeetGeek@users.noreply.github.com>
2024-02-13 12:54:44 +0530

    fix

--- src/components/page-specific/dashboard/2/Sales-analytics/index.js
+++
@@ -1,9 +0,0 @@
-function SalesAnalytics() {
-    return (
-        <>
-            <h1>SalesAnalytics</h1>
-        </>
-    );
-}
-
-export default SalesAnalytics;
---
+++ src/components/page-specific/dashboard/2/sales-ana/index.js
@@ -0,0 +1,9 @@
+function SalesAna() {
+    return (
+        <>
+            <h1>SalesAna</h1>
+        </>
+    );
+}
+
+export default SalesAna;
